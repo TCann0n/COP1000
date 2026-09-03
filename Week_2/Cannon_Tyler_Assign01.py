fb9e95a3053d145c9ea7ae9643ed166a6b7f5bfd
Tyler <tycann06@gmail.com>
2026-09-03 06:01:55 +0000

    Done

---
+++ Week_2/Cannon_Tyler_Assign01.py
@@ -0,0 +1,37 @@
+#Part 1, python code
+
+#inputs
+
+#Input statement for the studen't first and last name
+fullname =input("Enter your full name. ")
+
+#input statement prompting the student to enter their major now
+major = input("Enter your college major ")
+
+
+#Input statement asking the student what is one feature/concept that they want to learn and apply using python
+feature = input("What is one feature/concept you want to learn and apply using python? ")
+
+#input statement for asking the student what the dream job they want to obtain is
+job = input("What is your dream job that you wish to obtain? ")
+
+
+#print statements
+
+#just a print statement to add the borders, all future .center statements are set to (85) to match the amount of equal signs.
+print("=====================================================================================")
+
+#Printf statement that displays the student's full name
+print(f"The student's name is {fullname}".center(85))
+#printf statement that displays the entered major
+print(f"The student's major is {major}".center(85))
+#feature print statement
+print(f"The student wants to learn {feature}".center(85))
+#print statement displaying said job
+print(f"The student's dream job is {job}".center(85))
+
+
+
+
+#just a print statement to add the borders
+print("=====================================================================================")
