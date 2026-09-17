2eedac1bd7b106d2ebe7c2777d3a5d2ac651325e
Tyler <tycann06@gmail.com>
2026-09-17 08:00:48 +0000

    Added stuff

---
+++ Week4/Cannon_Tyler_Assign02.py
@@ -0,0 +1,21 @@
+#Programmer: Tyler Cannon
+#Program name: Assignment #02
+#Date written: 09/14/2026  
+#Purpose: Calculate and display the calculated number and percentages of big cats at the local zoo.
+
+#Part 1, Python code
+
+#inputs
+
+CustomerName = input("Enter your first and last name: ")
+Lions = input("Enter the number of Lions: ")
+Tigers = input("Enter the number of Tigers: ")
+
+#calculations
+
+Total = Lions + Tigers
+PercentLions = Lions/Total
+PercentTigers = Tigers/Total
+
+#Prints
+print("=" * 80)
